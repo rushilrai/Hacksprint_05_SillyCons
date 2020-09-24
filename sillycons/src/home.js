aa6de1fbc0b8e1c25cab94ce0f258be9af1c8749
Rushil Rai <58342835+rushilrai@users.noreply.github.com>
2020-09-25 05:07:55 +0530

    last

--- sillycons/src/home.js
+++ sillycons/src/home.js
@@ -12,6 +12,24 @@ class Home extends React.Component {
     }
 
     render() {
+        const teamnameStyle = {
+            fontFamily: 'Lato !important',
+            color: '#5786f8',
+            fontSize: '2.5vw',
+            fontWeight: '900'
+        }
+        const projectnameStyle = {
+            fontFamily: 'Lato !important',
+            color: 'black',
+            fontSize: '5vw',
+            fontWeight: '900'
+        }
+        const descStyle = {
+            fontFamily: 'Lato !important',
+            color: 'black',
+            fontSize: '2vw',
+            fontWeight: '700'
+        }
         var { isLoaded } = this.state;
 
         if (!isLoaded) {
@@ -20,7 +38,9 @@ class Home extends React.Component {
         else {
             return <div className='HomeStyle'>
                 <div className='HomeIntroStyle'>
-                    <h1>Introductory Content</h1>
+                    <h1 style={teamnameStyle}>Team SillyCons present,</h1>
+                    <h1 style={projectnameStyle}>Project Name</h1>
+                    <h1 style={descStyle}>Project Description</h1>
                 </div>
                 <div className='HomeStocksStyle'>
                     <StockCard />
