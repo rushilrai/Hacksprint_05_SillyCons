d98e2543f525bffa95f1af5ef7a66194bdcd6cd5
Rushil Rai <58342835+rushilrai@users.noreply.github.com>
2020-09-24 12:25:20 +0530

    Routing added to WebApp;

--- sillycons/src/App.js
+++ sillycons/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './home.js';
+import Dashboard from './dashboard.js';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <main>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/dashboard" component={Dashboard} exact />
+        <Route component={Error} />
+      </Switch>
+    </main>
   );
 }
 
