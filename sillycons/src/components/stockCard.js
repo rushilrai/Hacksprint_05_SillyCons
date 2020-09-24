afc0444f331621a4a8d3080b7babe7501c71f8c2
Rushil Rai <58342835+rushilrai@users.noreply.github.com>
2020-09-25 04:44:52 +0530

    update

--- sillycons/src/components/stockCard.js
+++ sillycons/src/components/stockCard.js
@@ -1,15 +1,9 @@
 import React from 'react';
+import '../styles/button.css'
+import '../styles/stockCard.css'
 
 class StockCard extends React.Component {
     render() {
-        const stockCardStyle = {
-            backgroundColor: 'white',
-            height: '200px',
-            width: '400px',
-            borderRadius: '10px',
-            boxShadow: '0px 0px 5px 2px #f5f5f6',
-            margin: '20px',
-        }
         const textBoxStyle = {
             margin: '10px',
         }
@@ -17,31 +11,68 @@ class StockCard extends React.Component {
             margin: '0px',
             fontFamily: 'Lato !important',
             color: 'grey',
+            fontSize: '1.5vw'
         }
         const nameStyle = {
             margin: '0px',
             fontFamily: 'Lato !important',
             color: 'black',
+            fontSize: '2vw',
+            fontWeight: '700'
+        }
+        const capStyle = {
+            fontFamily: 'Lato !important',
+            color: '#5786f8',
+            fontSize: '1.6vw',
+            fontWeight: '900'
         }
         const highStyle = {
-           // paddingTop: '5px',
+            // paddingTop: '5px',
             margin: '0px',
             fontFamily: 'Lato !important',
             color: 'green',
+            fontSize: '1.3vw'
         }
         const lowStyle = {
             //paddingTop: '5px',
             margin: '0px',
             fontFamily: 'Lato !important',
             color: 'red',
+            fontSize: '1.3vw'
         }
-        return <div style={stockCardStyle}>
+        const buttonRow = {
+            //border: '4px dotted blue',
+            justifyContent: 'space-between',
+            // marginRight: '10px',
+        }
+        const topRow = {
+            //border: '4px dotted blue',
+            justifyContent: 'space-between',
+        }
+        return <div className='stockCardStyle'>
             <div style={textBoxStyle}>
-                <h2 style={codeStyle}>MSFT</h2>
-                <h1 style={nameStyle}>Microsoft</h1>
+                <div className='row' style={topRow}>
+                    <div className='col-7'>
+                        <h2 style={codeStyle}>MSFT</h2>
+                        <h1 style={nameStyle}>Microsoft</h1>
+                    </div>
+                    <div className='col-4 my-auto'>
+                        <h2 style={capStyle}>
+                            $ 1.54T
+                        </h2>
+                    </div>
+                </div>
                 <br></br>
-                <h3 style={highStyle}>H: $205.57</h3>
-                <h3 style={lowStyle}>L: $199.20</h3>
+                <div className='row' style={buttonRow}>
+                    <div className='col-8'>
+                        <h3 style={highStyle}>H: $205.57</h3>
+                        <h3 style={lowStyle}>L: $199.20</h3>
+                    </div>
+                    <div className='col-4 my-auto'>
+                        <div className='buttonStyle'>Predict</div>
+                    </div>
+
+                </div>
             </div>
         </div>;
     }
--- sillycons/src/home.js
+++ sillycons/src/home.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import StockCard from './components/stockCard.js';
+import './styles/home.css'
 
 class Home extends React.Component {
     constructor(props) {
@@ -12,42 +13,16 @@ class Home extends React.Component {
 
     render() {
         var { isLoaded } = this.state;
-        const HomeStyle = {
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'space-between',
-            //alignItems: 'space-around',
-            //width: '100vw',
-           // border: '4px dotted blue',
-        }
-        const HomeStocksStyle = {
-            display: 'flex',
-            flexWrap: 'wrap',
-            height: '100vh',
-            width: '49vw',
-            alignContent: 'center',
-           // border: '4px dotted blue',
-        }
-        const HomeIntroStyle = {
-            display: 'flex',
-            flexWrap: 'wrap',
-            height: '100vh',
-            width: '40vw',
-            alignContent: 'center',
-            justifyContent: 'center',
-          //  border: '4px dotted blue',
-            fontFamily: 'Lato !important',
-        }
 
         if (!isLoaded) {
             return <center>....</center>
         }
         else {
-            return <div style={HomeStyle}>
-                <div style={HomeIntroStyle}>
+            return <div className='HomeStyle'>
+                <div className='HomeIntroStyle'>
                     <h1>Introductory Content</h1>
                 </div>
-                <div style={HomeStocksStyle}>
+                <div className='HomeStocksStyle'>
                     <StockCard />
                     <StockCard />
                     <StockCard />
