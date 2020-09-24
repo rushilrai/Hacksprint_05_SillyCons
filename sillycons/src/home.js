e09745e59fc728577c7779c3748c890544637515
Rushil Rai <58342835+rushilrai@users.noreply.github.com>
2020-09-24 17:35:02 +0530

    update web

--- sillycons/src/home.js
+++ sillycons/src/home.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import StockCard from './components/stockCard.js'
 
 class Home extends React.Component {
-  render() {
-      return <div><Link to='/dashboard'>Go to Dash</Link></div>;
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: true,
+        }
+    }
+
+  /*  componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(json => {
+                console.log(json);
+                this.setState({
+                    isLoaded: true,
+                })
+            })
+    } */
+
+
+    render() {
+        var { isLoaded} = this.state;
+
+        if (!isLoaded) {
+            return <center>....</center>
+        }
+        else {
+            return <div>
+                <Link to='/dashboard'>Go to Dash</Link>
+                <StockCard />
+            </div>;
+        }
+
+    }
 }
 
 export default Home;
